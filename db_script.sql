54463017572ded96b663afb9b671cce9c0f5c51d
bipinm25 <bipinm25@gmail.com>
2021-07-20 15:10:27 +0530

    script

--- db_script.sql
+++ db_script.sql
@@ -18,3 +18,7 @@ CREATE TABLE course_subscribe(
 	student_id INT NOT NULL,
 	course_id INT NOT NULL	
 ) ENGINE=INNODB;
+
+
+select concat(s.first_name,' ', s.last_name) full_name,c.course_name from course_subscribe cs left join students s on cs.student_id=s.id
+ LEFT JOIN courses c ON cs.course_id=c.id order by s.first_name
